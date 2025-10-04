bcf1dce1bad401ba735ea4429e54354a9511a659
artumont <197291181+artumont@users.noreply.github.com>
2025-10-04 00:27:17 -0600

    feat: initialize project with Tailwind CSS and Next.js setup
    - Added PostCSS configuration for Tailwind CSS.
    - Created SVG icons for various UI elements.
    - Added favicon for the application.
    - Implemented global CSS styles with Tailwind CSS.
    - Set up the main layout and metadata for the application.
    - Created the home page structure with a responsive layout.
    - Configured TypeScript settings for the project.

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Roboto_Flex } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto_Flex({
+    subsets: ["latin"],
+    variable: "--font-roboto",
+    display: "swap",
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+    title: "MontGovia",
+    description: "Somos Montgovia, un dúo que combina distintas habilidades para crear soluciones y proyectos útiles para empresas e individuos.",
+    icons: {
+        icon: "/favicon.ico",
+    },
+    openGraph: {
+        title: "MontGovia",
+        description: "Somos Montgovia, un dúo que combina distintas habilidades para crear soluciones y proyectos útiles para empresas e individuos.",
+        url: "https://montgovia.com",
+        siteName: "MontGovia",
+        images: [
+            {
+                url: "https://montgovia.com/og-image.png",
+                width: 1200,
+                height: 630,
+                alt: "MontGovia",
+            },
+        ],
+        locale: "es_ES",
+        type: "website",
+    },
+    };
+
+    export default function RootLayout({
+        children,
+    }: Readonly<{
+        children: React.ReactNode;
+    }>) {
+        return (
+            <html lang="en">
+                <body
+                    className={`${roboto.variable} antialiased`}
+                >
+                    {children}
+                </body>
+            </html>
+        );
+}
